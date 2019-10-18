fec27400f36155a94a6daf2377f741f1e8884468
xNJMSJMDCTx <jitin.k.verma2000@gmail.com>
2019-10-18 15:10:21 +0100

    Level 4 Complete

--- app/src/main/java/com/example/biggernumber/MainActivity.java
+++ app/src/main/java/com/example/biggernumber/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private int leftValue, rightValue;
     private int scoreValue = 0;
     TextView score;
+    private int maxLives = 3;
+    TextView lives;
     private Random r = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +28,18 @@ public class MainActivity extends AppCompatActivity {
         score = this.findViewById(R.id.scoreValue);
         score.setText(Integer.toString(scoreValue));
 
+        lives = this.findViewById(R.id.livesValue);
+        lives.setText(Integer.toString(maxLives));
+
         randomNumbers();
         leftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(leftValue > rightValue){
-                    scoreValue++;
-                    score.setText(Integer.toString(scoreValue));
-                    Toast.makeText(getApplicationContext(),"Congratulations", Toast.LENGTH_SHORT).show();
+                    increaseScore();
+                }
+                else{
+                    decreaseLives();
                 }
                 randomNumbers();
             }
@@ -42,9 +48,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(rightValue > leftValue){
-                    scoreValue++;
-                    score.setText(Integer.toString(scoreValue));
-                    Toast.makeText(getApplicationContext(),"Congratulations", Toast.LENGTH_SHORT).show();
+                    increaseScore();
+                }
+                else{
+                    decreaseLives();
                 }
                 randomNumbers();
             }
@@ -55,5 +62,23 @@ public class MainActivity extends AppCompatActivity {
         rightValue = r.nextInt(100);
         leftButton.setText(Integer.toString(leftValue));
         rightButton.setText(Integer.toString(rightValue));
+    }
+    public void increaseScore(){
+        scoreValue++;
+        score.setText(Integer.toString(scoreValue));
+        Toast.makeText(getApplicationContext(),"Congratulations", Toast.LENGTH_SHORT).show();
+    }
+    public void decreaseLives(){
+        if(maxLives > 1){
+            maxLives--;
+            lives.setText(Integer.toString(maxLives));
+        }
+        else{
+            maxLives = 0;
+            lives.setText(Integer.toString(maxLives));
+            leftButton.setEnabled(false);
+            rightButton.setEnabled(false);
+            Toast.makeText(getApplicationContext(),"Game Over", Toast.LENGTH_LONG).show();
+        }
     }
 }
